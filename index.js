3a4d6cfaacb0bb43454b7affd15d5ffea526d990
officialbube <ebubenkereebube@gmail.com>
2022-12-30 11:12:25 +0100

    nav environment

---
+++ index.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links a');
+    const header = document.querySelector('.header');
+
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+            
+        });
+
+
+        burger.classList.toggle('toggle');
+        header.classList.toggle('show');
+    });
+    
+    
+    
+}
+
+navSlide();
+
